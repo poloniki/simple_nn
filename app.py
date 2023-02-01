b51be8cb8faac8239184d0c2ab9841554ef2aa4e
Nikita Polovinkin <poloniki@ya.ru>
2023-02-01 17:57:41 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import dash
+from dash import html
+from dash import dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+from datetime import datetime
+import plotly.graph_objects as go
+import pandas as pd
+from dash import Dash, dash_table, dcc, html
