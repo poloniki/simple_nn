879537493f98f1143fbd5b137a73f83abe1da2fa
Nikita Polovinkin <poloniki@ya.ru>
2023-02-15 18:08:11 +0100

    Comment out graph for time

--- pages/RNN.py
+++ pages/RNN.py
@@ -3,26 +3,26 @@ import pandas as pd
 import numpy as np
 import graphviz
 import random
-g1 = graphviz.Digraph(graph_attr={'rankdir':'LR', 'TBbalance':"max"})
+# g1 = graphviz.Digraph(graph_attr={'rankdir':'LR', 'TBbalance':"max"})
 
-g1.attr('node', shape='circle')
-g1.node('X')
-g1.node('h_prev')
-g1.node('U')
-g1.node('W')
-g1.node('h')
-g1.node('V')
-g1.node('y')
+# g1.attr('node', shape='circle')
+# g1.node('X')
+# g1.node('h_prev')
+# g1.node('U')
+# g1.node('W')
+# g1.node('h')
+# g1.node('V')
+# g1.node('y')
 
-g1.edge('X', 'U', label='input\ntransform')
-g1.edge('h_prev', 'W', label='hidden\ntransform')
-g1.edge('U', 'h', label='input\nhidden')
-g1.edge('W', 'h', label='prev\nhidden')
-g1.edge('h', 'V', label='hidden\noutput')
-g1.edge('V', 'y', label='output\ntransform')
+# g1.edge('X', 'U', label='input\ntransform')
+# g1.edge('h_prev', 'W', label='hidden\ntransform')
+# g1.edge('U', 'h', label='input\nhidden')
+# g1.edge('W', 'h', label='prev\nhidden')
+# g1.edge('h', 'V', label='hidden\noutput')
+# g1.edge('V', 'y', label='output\ntransform')
 
-g1.render('vanilla_rnn')
-st.graphviz_chart(g1)
+# g1.render('vanilla_rnn')
+# st.graphviz_chart(g1)
 
 
 
